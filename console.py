5f87935f0ef9a169d07698afb4886d9ea6205849
jeffmakuto <hjeffmakuto@proton.me>
2023-10-15 12:33:50 -0400

    console 0.0.1

---
+++ console.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""modules for the command interpreter"""
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """ Defines the command interpreter
+    Attributes:
+        prompt (str): The command prompt.
+    """
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """ Quit command to exit the program. """
+        return True
+
+    def do_EOF(self, arg):
+        """ EOF command to exit the program. """
+        print("")
+        return True
+
+    def emptyline(self):
+        """ Do nothing on empty line. """
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
