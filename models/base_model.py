da6b2dfc4795cd520da80fbf64a4eb612a93a5f2
jeffmakuto <hjeffmakuto@proton.me>
2023-10-15 10:31:49 -0400

    base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,72 +1,49 @@
 #!/usr/bin/python3
 
-"""
-Modules for base model.
-"""
+"""Module defining all common attributes/methods for other classes"""
 
-from datetime import datetime
-import json
-import uuid
 import models
-
+from datetime import datetime
+import uuid
 
 class BaseModel:
-    """
-    class that defines all common attributes and methods for other classes.
-    """
-    def __init__(self, **kwargs):
-        """Initialize a new BaseModel instance with the given attributes."""
+    """ Defines common attributes/methods for other classes
+        Public Instance Attributes:
+            id: (string) - assigned with a uuid when an instance is created.
+            created_at: (dtatetime) - assigned with the current datetime when
+                        an instance is created
+            updated_at: (datetime) - assigned with current datetime when an
+                                    instance is created and will be updated
+                                    every time the object changes
+            __str__: prints: [<class name>] (<self.id>) <self.__dict__>
 
-        # Set a unique ID for the instance
+        Public Instance Methods:
+            save(self): updates the public instance attribute updated_at with
+                        the current datetime
+            to_dict(self): returns a dictionary containing all keys/values of
+                            __dict__ of the instance.
+    """
+    def __init__(self, *args, **kwargs):
+        """ Instantiate objects """
         self.id = str(uuid.uuid4())
-
-        # Set the creation and update dates to the current date and time
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
-        # Set the date string format
-        tform = "%Y-%m-%dT%H:%M:%S.%f"
-
-        # Update the instance attributes based on the given keyword arguments
-        if kwargs:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    setattr(self, k, datetime.strptime(v, tform))
-                else:
-                    setattr(self, k, v)
-        else:
-            models.storage.new(self)
-
     def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
-        # Get the class name and instance ID
-        clname = type(self).__name__
-        instance_id = getattr(self, "id", None)
-        # Get a dictionary of the instance attributes
-        instance_attrs = vars(self).copy()
-        # Remove any reserved attributes
-        reserved_attrs = ["id", "created_at", "updated_at"]
-        for attr in reserved_attrs:
-            instance_attrs.pop(attr, None)
-        # Format the string representation of the instance
-        instance_attrs_str = ", ".join(f"{k}={v!r}"
-                                       for k, v in instance_attrs.items())
-        instance_str = f"[{clname}] ({instance_id}) {{{instance_attrs_str}}}"
-        return instance_str
-
+        """ Returns a formatted string """
+        return "[{}] ({}) {}".format(self.__class__.name__,
+                                     self.id, self.__dict__)
     def save(self):
-        """
-        Updates the attribute 'updated_at' to current date and time.
+        """ Updates the public instance attribute updated_at with the
+            current datetime
         """
         self.updated_at = datetime.now()
-        models.storage.save()
-
     def to_dict(self):
+        """ Returns a dictionary containing all keys/values of __dict__ of
+            of the instance
         """
-        Returns dictionary representation of instance of base model.
-        """
-        base_dict = self.__dict__.copy()
-        base_dict["__class__"] = type(self).__name__
-        base_dict["created_at"] = base_dict["created_at"].isoformat()
-        base_dict["updated_at"] = base_dict["updated_at"].isoformat()
-        return base_dict
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
